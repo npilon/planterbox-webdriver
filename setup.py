2d35294a0c41b955230c939bd24c8b36499471d3
Nick Pilon <npilon@lexmachina.com>
2016-01-28 10:18:27 -0800

    Release 0.3.2

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 requires = [
-    "planterbox>=0.4.1",
+    "planterbox>=0.5",
     "selenium",
 ]
 
 description = 'Steps for web testing using selenium and planterbox'
 
 setup(name='planterbox-webdriver',
-      version='0.3.1',
+      version='0.3.2',
       description=description,
       classifiers=[
           "Intended Audience :: Developers",
